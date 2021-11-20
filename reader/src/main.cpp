fd33d46b5e057f448497dca67adb1acc27735489
Michael Maguire <xcode@michaelmaguire.ca>
2021-11-20 13:45:24 +0100

    Get boost::log set up to write to a file

--- reader/src/main.cpp
+++ reader/src/main.cpp
@@ -8,36 +8,50 @@
 
 #include <iostream>
 #include <boost/log/trivial.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/severity_logger.hpp>
 #include <gtest/gtest.h>
 
 using namespace std;
+using namespace boost::log::trivial;
 
-#if 0
-void init_logging()
+void init_logging(const std::string & filename)
 {
-    logging::add_file_log("sample.log");
-
-    logging::core::get()->set_filter
+    boost::log::add_file_log
     (
-        logging::trivial::severity >= logging::trivial::info
+    	boost::log::keywords::file_name = filename + "_%N.log",
+		boost::log::keywords::rotation_size = 10 * 1024 * 1024,
+		boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0),
+		boost::log::keywords::format = "[%TimeStamp% p[%ProcessID%] t[%ThreadID%]]: %Message%"
     );
+
+    boost::log::core::get()->set_filter
+    (
+        boost::log::trivial::severity >= boost::log::trivial::info
+    );
+
+    boost::log::add_common_attributes();
 }
-#endif
 
 
 GTEST_API_ int main(int argc, char **argv) {
-	//init_logging();
-	cout << "argc[" << argc << "]" << endl;
 	if( argc > 0 ) {
 	    BOOST_LOG_TRIVIAL(trace) << "argv[0][" << argv[0] << "]";
 	}
+	init_logging("reader");
+
+	boost::log::sources::severity_logger< boost::log::trivial::severity_level > lg;
+
 	if( argc > 1 ) {
-		cout << "argv[1][" << argv[1] << "]" << endl;
+		BOOST_LOG_SEV(lg, info) << "argv[1][" << argv[1] << "]";
 		if( 0 == strcmp(argv[1], "--test" ) ) {
-			cout << "Running " << argv[0] << " main() from main.cpp RUN_ALL_TESTS starting\n" << endl;
+			BOOST_LOG_SEV(lg, info) << "Running " << argv[0] << " main() from main.cpp RUN_ALL_TESTS starting\n";
 			testing::InitGoogleTest(&argc, argv);
 			int returnValue = RUN_ALL_TESTS();
-			cout << "Running " << argv[0] << " main() from main.cpp RUN_ALL_TESTS returnValue[" << returnValue << "]\n" << endl;
+			BOOST_LOG_SEV(lg, info) << "Running " << argv[0] << " main() from main.cpp RUN_ALL_TESTS returnValue[" << returnValue << "]\n";
 			return returnValue;
 		}
 	}
