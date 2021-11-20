72e1bc4e1c0fc22a52ce7fa6ac86d4dc5c5a3e24
Michael Maguire <xcode@michaelmaguire.ca>
2021-11-20 12:14:37 +0100

    Initial setup

---
+++ reader/src/reader.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : reader.cpp
+// Author      : michaelmaguire
+// Version     :
+// Copyright   : Copyright michaelmaguire
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
