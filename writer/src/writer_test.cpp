e671bde658dc209ce4eddffa46950664b2a2c9d8
Michael Maguire <xcode@michaelmaguire.ca>
2021-11-21 01:58:53 +0100

    test multiple writes

--- writer/src/writer_test.cpp
+++ writer/src/writer_test.cpp
@@ -7,15 +7,16 @@ TEST(ProtobufWriterSuite, sendNewOrder) {
 
 	ProtoWriter protowriter(1234);
 
-	exchange::ExchangeMessage exchangeMessage;
-	auto newOrder = new exchange::NewOrder();
-	newOrder->set_user(1);
-	newOrder->set_symbol("IBM");
-	newOrder->set_price(10);
-	newOrder->set_quantity(100);
-	newOrder->set_side(exchange::NewOrder_SideType::NewOrder_SideType_BUY);
-	newOrder->set_userorder(1);
-	exchangeMessage.set_allocated_neworder(newOrder);
-
-	protowriter.send(exchangeMessage);
+	for( uint32_t order = 0; order < 100; ++order ) {
+		exchange::ExchangeMessage exchangeMessage;
+		auto newOrder = new exchange::NewOrder();
+		newOrder->set_user(1);
+		newOrder->set_symbol("IBM");
+		newOrder->set_price(10);
+		newOrder->set_quantity(100);
+		newOrder->set_side(exchange::NewOrder_SideType::NewOrder_SideType_BUY);
+		newOrder->set_userorder(order);
+		exchangeMessage.set_allocated_neworder(newOrder);
+		protowriter.send(exchangeMessage);
+	}
 }
